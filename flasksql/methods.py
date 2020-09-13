6a0ccff0915d20be29d60b976474dbfb9adf6c9c
YoureaBosonHarry <rath3.14159@protonmail.com>
2020-09-12 23:45:07 -0500

    made changes

--- flasksql/methods.py
+++ flasksql/methods.py
@@ -1,15 +1,32 @@
 import requests
 import random
+import rolltide
+
+# This is using the completely wrong library
+# Use flask not requests
+
+# app = flask.Flask(__name)__)
+# @app.route('/roll', methods=['GET', 'POST'])
+# def num_of_rolls():
+#     num_of_dice = int(request.args.get('numOfDice')
+#
+#
+#     ... Finish the rest using the rolltide library you've created
+#     This will require you to rewrite the input statement such that the
+#     dice sides and num of dice can be passed as a function argument
+#     return an hashmap object like {'1': 2, '2': 5, '3': 6, '4': 1, '5': 0, '6': 3}
+#     where the values of the dictionary are the number of rolls, so this should sum to the number
+#     of dice given by the function argument
+#     Before trying to write these to a database, first try returning them to the client via a return statement in the flask method
 
 @bp.route('/roll', methods=['GET', 'POST'])
 
-def numOfRolls():
+def numOfRolls(): # Dont mix case syntax
 
     arg = int(request.args.get('VAL'))
 
-    for i in range(0,arg):
-        roll = random.randint(1,6)
-
+    rolltide(num_of_dice, num_of_side)
+    
     if request.method == 'POST':
         return print(f'You rolled a {roll}')
 
